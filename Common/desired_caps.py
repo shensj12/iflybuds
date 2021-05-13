d9de41b8c8edd7bbc953aaf0ec65fdea8da1d8b5
shensj12 <72734183+shensj12@users.noreply.github.com>
2021-05-13 18:40:07 +0800

    配置中增加newCommandTimeout解决默认超时时间60s，超时后应用将会自动关闭session问题

--- Common/desired_caps.py
+++ Common/desired_caps.py
@@ -21,21 +21,22 @@ def appium_desired():
     desired_caps['platformName']=data['platformName']
     desired_caps['platformVersion']=str(data['platformVersion'])
     desired_caps['deviceName']=data['deviceName']
-
     #os.path.dirname(__file__)返回脚本的路径，即文件路径中所在的目录（不包含文件名）
     base_dir = os.path.dirname(os.path.dirname(__file__))
     # os.path.join()连接两个或多个路径名
     app_path = os.path.join(base_dir, 'App', data['appname'])
     desired_caps['app']=app_path
-
     desired_caps['appPackage']=data['appPackage']
     desired_caps['appActivity']=data['appActivity']
     desired_caps['noReset']=data['noReset']
-
     desired_caps['unicodeKeyboard']=data['unicodeKeyboard']
     desired_caps['resetKeyboard']=data['resetKeyboard']
+    desired_caps['newCommandTimeout'] = data['newCommandTimeout']
 
     logging.info('start app...')
+
+
+
     driver=webdriver.Remote('http://'+str(data['ip'])+':'+str(data['port'])+'/wd/hub',desired_caps)
     driver.implicitly_wait(8)
     return driver
